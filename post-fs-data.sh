5ee2efc6c37aa8eb95eb775399641c3495daf8f9
Lucia Fuskova <84447174+Lucia361@users.noreply.github.com>
2024-01-02 11:11:27 +0400

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,10 +1,13 @@
 #!/system/bin/sh
+# Mount system certificates to the new location for Android 14 compatibility
+mount -o bind /system/etc/security/cacerts /apex/com.android.conscrypt/cacerts
+
 # Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
+# This will make your scripts compatible even if Magisk changes its mount point in the future
 MODDIR=${0%/*}
 
 mkdir -p $MODDIR/system/etc/security/cacerts
-rm $MODDIR/system/etc/security/cacerts/*
+rm -rf $MODDIR/system/etc/security/cacerts/*
 cp -f /data/misc/user/0/cacerts-added/* $MODDIR/system/etc/security/cacerts/
 set_perm_recursive $MODDIR/system/etc/security/cacerts/ root root 644
 
